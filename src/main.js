e698abcada08a93e6cca7b641332efce6f70018b
Sojourner1066 <morgangrundy@gmail.com>
2025-03-22 09:59:05 +0000

    creating branch to move files from other project

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { Deck } from '@deck.gl/core';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { LeafletDeckGl } from '@deck.gl/leaflet';
 
-setupCounter(document.querySelector('#counter'))
+// 1. Initialize Leaflet map
+const map = L.map('map').setView([37.7749, -122.4194], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// 2. Create a deck.gl layer (example: points)
+const scatterplot = new ScatterplotLayer({
+  id: 'scatterplot-layer',
+  data: [
+    { position: [-122.4194, 37.7749], size: 100 },
+    { position: [-122.4294, 37.7849], size: 150 }
+  ],
+  getPosition: d => d.position,
+  getRadius: d => d.size,
+  getColor: [255, 0, 0],
+  radiusScale: 1,
+  pickable: true
+});
+
+// 3. Attach deck.gl to Leaflet
+new LeafletDeckGl({ layers: [scatterplot] }).addTo(map);
