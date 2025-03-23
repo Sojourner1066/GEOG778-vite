11c24f4131bf355a5cd0505cd53399865f49776f
Sojourner1066 <morgangrundy@gmail.com>
2025-03-23 19:05:24 +0000

    updated to used deck layer

--- src/updateDeckLayers.js
+++ src/updateDeckLayers.js
@@ -3,16 +3,8 @@ import { MapView } from '@deck.gl/core';
 import { ArcLayer } from '@deck.gl/layers';
 import { Deck } from '@deck.gl/core';
 
-export function updateDeckLayer(map, countryCentroids, selectedCountries, selectedCountryISO3) {
-    // Remove existing deck.gl layers
-    console.log(map); // Debug the map object
-    map.eachLayer(layer => {
-        if (layer._isDeckLayer) {
-            map.removeLayer(layer);
-        }
-    });
-
-    // Create a new deck.gl ArcLayer
+export function updateDeckLayer(deck, countryCentroids, selectedCountries, selectedCountryISO3) {
+    // Create a new ArcLayer
     const arcLayer = new ArcLayer({
         id: 'arcs',
         data: countryCentroids,
@@ -35,37 +27,8 @@ export function updateDeckLayer(map, countryCentroids, selectedCountries, select
         getWidth: 1
     });
 
-    // Create a custom Leaflet layer for deck.gl
-    const deckLayer = L.layerGroup(); // Create an empty Leaflet layer group
-
-    const deckInstance = new Deck({
-        canvas: 'deck-canvas', // ID of the canvas element
-        views: [new MapView({ repeat: true })],
-        layers: [arcLayer],
-        initialViewState: {
-            longitude: 0,
-            latitude: 0,
-            zoom: 2
-        },
-        controller: true
+    // Update the Deck instance with the new layer
+    deck.setProps({
+        layers: [arcLayer] // Replace any existing layers with the new ArcLayer
     });
-
-    // Add the deck.gl canvas to the map
-    const deckCanvas = document.getElementById('deck-canvas');
-    if (!deckCanvas) {
-        const canvas = document.createElement('canvas');
-        canvas.id = 'deck-canvas';
-        canvas.style.position = 'absolute';
-        canvas.style.top = '0';
-        canvas.style.left = '0';
-        canvas.style.width = '100%';
-        canvas.style.height = '100%';
-        map.getContainer().appendChild(canvas);
-    }
-
-    // Add the custom layer to the map
-    map.addLayer(deckLayer);
-
-    // Tag the layer for easy removal
-    deckLayer._isDeckLayer = true;
 }
