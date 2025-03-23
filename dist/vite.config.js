cdccd106c33a979c5495f74484e3c2bfb5ef16c9
Sojourner1066 <morgangrundy@gmail.com>
2025-03-23 10:53:09 +0000

    moved vite.config back to root and created config.js

--- dist/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/GEOG778-vite/',
-});
-// public/config.js
-window.config = {
-  MAPBOX_TOKEN: "pk.eyJ1IjoibXdpc2NnIiwiYSI6ImNtN2VtbGEzNzBnaTgyam9vZXl3YzM2Ym4ifQ.YJe4CFT-CEYhl0D98Wk8aw"
-};
