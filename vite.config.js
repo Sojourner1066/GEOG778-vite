1c37128143ebb1f69ffce3a7452f1871d3ed6647
Sojourner1066 <morgangrundy@gmail.com>
2025-03-31 18:52:12 +0100

    installing react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/GEOG778-vite/',
+  plugins: [react()],
   optimizeDeps: {
     include: ['deck.gl', 'deck.gl-leaflet', 'leaflet']
   },
